bf96d138244943d6d5f437e824a6cc3c657b4419
GIRISHMESH <75659142+GIRISHMESH@users.noreply.github.com>
2025-10-06 18:27:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.13"
   
 required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 2.0" }
-    azuread = { source = "hashicorp/azuread", version = "~> 1.0" }
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
+    azuread = { source = "hashicorp/azuread", version = "~> 2.0" }
     random  = { source = "hashicorp/random", version = "~> 3.0" }
   }
   
@@ -19,5 +19,6 @@ backend "azurerm" {
 provider "azurerm" {
   features {}
 }
+
 
 
